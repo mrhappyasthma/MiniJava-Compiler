bc426e53e8e883f5a26a6f59b4a6b345966550f9
mrhappyasthma <mrhappyasthma@gmail.com>
2015-04-03 23:10:51 -0400

    Added a temporary sample to test the backwards statements

---
+++ samples/BackwardsTest.java
@@ -0,0 +1,17 @@
+class Factorial{
+	public static void main(String[] a){
+		System.out.println(new Fac().ComputeFac(10));
+	}
+}
+
+class Fac {
+    public int ComputeFac(){
+	int x;
+	{
+		x = 3;
+		x = 4;
+		x = 5;
+	}
+	return 0 ;
+    }
+}
