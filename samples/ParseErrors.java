20b89df81a44c83471402eb68696d83fee33cf7e
mrhappyasthma <mrhappyasthma@gmail.com>
2015-04-05 22:47:56 -0400

    Updated error checking examples

--- samples/ParseErrors.java
+++ samples/ParseErrors.java
@@ -1,39 +1,31 @@
 class Factorial{
 	public static void main(String[] args){
-		System.out.println.(new Fac().ComputeFac());
+		System.out.println.(new Fac().ComputeFac());  //Statement Error: Line 3
 	}
 }
 
 class Fac{
-	public int ErrorTime (int){
+	public int ErrorTime (int){   //Formal Parameter Error: Line 8
 	}
     public int ComputeFac (int num){
 	int y;
-	int  ; 
+	int  ;         //Variable Declaration Error: line 12
 	if (num < 1)
 	    num_aux = 1 ;
 	else 
-	    num_aux = num * (this.ComputeFac num-1)) ;
+	    num_aux = num * (this.ComputeFac num-1)) ;  //Actual Parameter Error: line 16
 	return num_aux ;
     }
 }
 
-Foo{
+Foo{  //Class Body Error: Line 21
 	public int bar(){
 		return 0;
 	}
 }
 
 class Error{
-	public  Foo (int num){
+	public  Foo (int num){   //Method Body Error: Line 28
 	return 1 ;
     }
 }
-
-// Errors: 
-//			Statement - Line 3
-//			Formal Parameter - Line 8
-//          Variable Declaration - line 12
-//			Actual Parameter - line 16
-//			Class Body - Line 21
-//			Method Body - Line 28
--- samples/TypeChecking.java
+++ samples/TypeChecking.java
@@ -1,18 +1,30 @@
-class Factorial{
+class TestClass{
 	public static void main(String[] a){
-		System.out.println(new Fac().ComputeFac(10));
+		System.out.println(this);    //"this" in a static method:  line 3
 	}
 }
 
-class Fac {
-    public int ComputeFac(int num){
+class SecondClass {
+    public int Foo(int num){
 	int num_aux; 
-	if (num + 1)
-	    num_aux = 1 ;
-	else 
-	    num_aux = num * (this.ComputeFac(num-1)) ;
-	return num_aux ;
+	int num_aux;                    //Redefined Variable: line 10
+	boolean b;
+	int[] x;
+	
+	fakevar = 3;                    //Undefined Variable: line 14
+	b = SecondClass;                //Invalid r-value: line 15
+	num_aux = this.Foo();           //Wrong number of params:  line 16
+	num_aux = this.Foo(false);      //Wrong types of params:  line 17
+	num_aux = 1 + SecondClass;      //Invalid operands:  line 18
+	num_aux = 1 + false;            //Non-integer operand:  line 19
+	b = 3 && true;                  //Boolean operator on non-boolean operand:  line 20
+	num_aux = b.length;             //.length on non-aarry:  Line 21
+	if(1){							//Non-boolean expr in statement:  Line 22
+		System.out.println(1);
+	} else {
+		System.out.println(2);
+	}
+	num_aux = false;                //Type mismatch:  Line 27
+	return num_aux;
     }
 }
-
-
