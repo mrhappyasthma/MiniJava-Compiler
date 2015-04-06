504d3959571bbed2d06b986c12c6e510ff96aba9
mrhappyasthma <mrhappyasthma@gmail.com>
2015-04-05 23:23:01 -0400

    Updated TypeChecking.java to contain one more type of error

--- samples/TypeChecking.java
+++ samples/TypeChecking.java
@@ -12,19 +12,20 @@ class SecondClass {
 	int[] x;
 	
 	fakevar = 3;                    //Undefined Variable: line 14
-	b = SecondClass;                //Invalid r-value: line 15
-	num_aux = this.Foo();           //Wrong number of params:  line 16
-	num_aux = this.Foo(false);      //Wrong types of params:  line 17
-	num_aux = 1 + SecondClass;      //Invalid operands:  line 18
-	num_aux = 1 + false;            //Non-integer operand:  line 19
-	b = 3 && true;                  //Boolean operator on non-boolean operand:  line 20
-	num_aux = b.length;             //.length on non-aarry:  Line 21
-	if(1){							//Non-boolean expr in statement:  Line 22
+	SecondClass = 3;				//Invalid l-value: line 15
+	b = SecondClass;                //Invalid r-value: line 16
+	num_aux = this.Foo();           //Wrong number of params:  line 17
+	num_aux = this.Foo(false);      //Wrong types of params:  line 18
+	num_aux = 1 + SecondClass;      //Invalid operands:  line 19
+	num_aux = 1 + false;            //Non-integer operand:  line 20
+	b = 3 && true;                  //Boolean operator on non-boolean operand:  line 21
+	num_aux = b.length;             //.length on non-aarry:  Line 22
+	if(1){							//Non-boolean expr in statement:  Line 23
 		System.out.println(1);
 	} else {
 		System.out.println(2);
 	}
-	num_aux = false;                //Type mismatch:  Line 27
+	num_aux = false;                //Type mismatch:  Line 28
 	return num_aux;
     }
 }
