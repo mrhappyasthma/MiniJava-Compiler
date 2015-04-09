1cf66d4582767d549e5a2b261a8d30c1e43022e9
tayanemoura <tayanemoura@id.uff.br>
2015-04-08 23:10:31 -0400

    a function that returns reg temporaries

--- regalloc/RegisterAllocator.java
+++ regalloc/RegisterAllocator.java
@@ -2,13 +2,35 @@ package regalloc;
 
 public class RegisterAllocator
 {
+	int[] temporaries;
+	int qtdTemp;
+
 	public RegisterAllocator()
 	{
-		
+		temporaries = new int[10];
+		qtdTemp = 0;
 	}
 	
-	public String allocate()
+	public String allocateTemporaries(int num)
 	{
-		return null;
+		int i;
+		if(qtdTemp==10){
+			//System.out.println("");
+			//System.exit(0);
+			return null;
+		}
+		//verify if the value already exists
+		for(i=0; i<10; i++)
+		{
+			if(temporaries[i]==num)
+			{
+					return("$t"+i);
+			}
+		}
+		temporaries[qtdTemp]=num;
+		int reg = qtdTemp;
+		qtdTemp++;
+		return("$t"+reg);
+		
 	}
 }
