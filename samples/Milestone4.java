f491ed834da976d888d92284888a9063bd0bd051
mrhappyasthma <mrhappyasthma@gmail.com>
2015-04-17 18:05:33 -0400

    Updated milestone4.java test file

--- samples/Milestone4.java
+++ samples/Milestone4.java
@@ -1,27 +1,32 @@
 class Test {
 	public static void main(String[] args) {
-		System.out.println(new Test2().Start(0));
+		System.out.println(new Test2().Start(0));  //Prints -25
 	}
 }
 class Test2 {
 	public int Start(int y) {
-		//Arbitrary straight-line code -- do nto use registers $zero, $a0-a3, $v0, or $sp/$fp
+		//Arbitrary straight-line code -- do not use registers $zero, $a0-a3, $v0, or $sp/$fp
 		int a;
 		int b;
-		int c;
-		int d;
+		
+		boolean b1;
+		boolean b2;
 		
 		a = 3;
-		b = a;
-		c = a + b;
-		c = c + 1;
-		c = 1 + c;
-		d = c - 1 - 1;
-		d = 1 - d;
-		d = d * d;
-		d = -1 * d;
-		d = d * -1;
+		a = a + a;
+		a = a + 1;
+		a = 1 + a;
+		a = a - 1 - 1;
+		a = 1 - a;
+		a = a * a;
+		a = -1 * a;
+		b = b * -1;
+		b = 3 * 3;
 		
-		return d;
+		b1 = true;
+		b2 = false;
+		b1 = b1 && b2;
+		
+		return a;
 	}
 }
