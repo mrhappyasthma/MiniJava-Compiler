f9d2c2dcf52396c693afdda4824f8283bb2d342a
mrhappyasthma <mrhappyasthma@gmail.com>
2015-04-19 18:42:02 -0400

    Updated the test file for Milestone9 (objects/arrays)

--- samples/Milestone9.java
+++ samples/Milestone9.java
@@ -3,21 +3,28 @@ class Test {
 		System.out.println(new Test2().Start(0));  //Prints 3 (length)
 	}
 }
-class Test2 {
+class Test2 extends Test3 {
+	int d;
+	int[] e;
 	public int Start(int y) {
-		int[] a;
-		int x;
+		e = new int[3];
+		a = 3;
+		d = 4;
+		e[0] = a;
+		e[1] = d;
+		e[2] = e[0] + e[1];
 		
-		a = new int[3];
-		x = 3;
-		a[0] = 7;
-		a[1] = x;
-		a[2] = a[0] + a[1];
+		System.out.println(e[2]); //Prints 7
 		
-		x = a[0];
-
-		System.out.println(a[0] + a[0]); //Prints 14
-		
-		return a.length;
+		return e.length;
 	}
+}
+
+class Test3 extends Test4{
+	int b;
+	boolean c;
+}
+
+class Test4{
+	int a;
 }
