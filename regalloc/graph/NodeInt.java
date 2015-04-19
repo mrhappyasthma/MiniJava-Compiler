1f1339799b370aae61c9bc81ea0decbca43f1a3e
tayanemoura <tayanemoura@id.uff.br>
2015-04-19 15:29:43 -0400

    with functions that are going to help when coloring

--- regalloc/graph/NodeInt.java
+++ regalloc/graph/NodeInt.java
@@ -1,6 +1,7 @@
 
 package regalloc.graph;
 
+import java.util.LinkedList;
 import java.util.List;
 import symboltable.Variable;
 
@@ -10,18 +11,31 @@ public class NodeInt {
     
     public NodeInt(Variable v){
         var = v;
+        edge = new LinkedList<NodeInt>();
+       
     }
     
     public void addEdge(NodeInt e){
         //if the edge doesn't exist yet we add it 
-        if(!edge.contains(e)){
+        if(!containsEdge(e)){
             edge.add(e);
         }
+    }
+    public void rmEdge(NodeInt e){
+        edge.remove(e);
+    }
+    
+    public boolean containsEdge(NodeInt e){
+        if(!edge.contains(e)){
+            return false;
+        }
+        return true;
     }
     
     public List<NodeInt> getEdge(){
         return edge;
     }
+    
     
     public Variable getVar(){
         return var;
